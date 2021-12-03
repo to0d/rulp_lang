2f70fa9459cf275c2f2e981838ce161ff99e3740
todd-t4 <lylybj@cn.ibm.com>
2021-12-03 11:50:34 +0800

    up

--- test/beta/rulp/rclass/XRFactorNewTest.java
+++ test/beta/rulp/rclass/XRFactorNewTest.java
@@ -7,9 +7,16 @@ import alpha.rulp.utils.RulpTestBase;
 class XRFactorNewTest extends RulpTestBase {
 
 	@Test
-	public void test_err_1_duplicate_obj() {
+	public void test_1_err_duplicate_obj() {
 		_setup();
 		_test_script();
+	}
+
+	@Test
+	public void test_2_no_instance_name() {
+		_setup();
+		_test_script();
+		_gInfo();
 	}
 
 }
