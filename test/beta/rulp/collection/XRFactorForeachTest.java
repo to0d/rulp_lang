2266c68588938abf83cc2ea0523982f77d5936ee
todd <to0d@outlook.com>
2021-12-12 10:56:25 +0800

    up

--- test/beta/rulp/collection/XRFactorForeachTest.java
+++ test/beta/rulp/collection/XRFactorForeachTest.java
@@ -10,20 +10,13 @@ class XRFactorForeachTest extends RulpTestBase {
 	void test_foreach_1() {
 
 		_setup();
-		_test("(foreach (?a '(1 2 3)) (+ ?a 1))", "'(2 3 4)");
-		_test("(foreach (?a '(1 2 3)) (+ ?a 1) (+ ?a 2))", "'(3 4 5)");
-		_test("(foreach (?a '(1 2 3 4 5)) (if (> ?a 3) (return 1)) (return ?a))", "'(1 2 3 1 1)");
-		_test("(foreach (?a '(1 2 3 4 5)) (if (= ?a 3) (continue)) (return ?a))", "'(1 2 4 5)");
-		_test("(foreach (?a '(1 2 3 4 5)) (if (= ?a 3) (break)) (return ?a))", "'(1 2)");
-		_test("(foreach (?a '(1 2 3)) (return '(?a 1)))", "'('(1 1) '(2 1) '(3 1))");
+		_run_script();
 	}
 
 	@Test
-	void test_foreach_2() {
+	void test_foreach_2_from_fun() {
 
 		_setup();
-		_test("(defun fun1 () (return '(1 2 3)))", "fun1");
-		_test("(name-of fun1)", "\"(fun1)\"");
-		_test("(foreach (?a (fun1)) (+ ?a 1))", "'(2 3 4)");
+		_run_script();
 	}
 }
