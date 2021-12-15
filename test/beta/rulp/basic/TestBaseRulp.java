b66572ac37a6b7eecacf834b802dcf83fb988fd1
todd-t4 <lylybj@cn.ibm.com>
2021-12-15 14:35:12 +0800

    up

---
+++ test/beta/rulp/basic/TestBaseRulp.java
@@ -0,0 +1,21 @@
+package beta.rulp.basic;
+
+import org.junit.Test;
+
+import alpha.rulp.utils.RulpTestBase;
+
+public class TestBaseRulp extends RulpTestBase {
+
+	@Test
+	public void test_base_1_println() {
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	public void test_base_2_inc_dec() {
+		_setup();
+		_run_script();
+	}
+
+}
