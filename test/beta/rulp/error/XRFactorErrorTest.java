5b0d07f1ee817484589fc20da114d2fc27f7f36b
todd-t4 <lylybj@cn.ibm.com>
2021-12-20 17:28:07 +0800

    up

--- test/beta/rulp/error/XRFactorErrorTest.java
+++ test/beta/rulp/error/XRFactorErrorTest.java
@@ -7,21 +7,20 @@ import alpha.rulp.utils.RulpTestBase;
 class XRFactorErrorTest extends RulpTestBase {
 
 	@Test
-	void test_1() {
+	void test_error_2_user_define_handle_case() {
 		_setup();
-		_test("(try (if (> 2 1) (error e1 \"msg1\") (error e2 \"msg2\"))" + " (e1 (print e1)) (e2 (print e2)))", "nil",
-				"error: e1, msg1");
+		_run_script();
 	}
 
 	@Test
-	void test_default_handle_case() {
+	void test_error_1_default_handle_case() {
 
 		_setup();
-		_test("(try (if (> 2 3) (error e1 \"msg1\") (error e2 \"msg2\")) (?e (print ?e)))", "nil", "error: e2, msg2");
+		_run_script();
 	}
 
 	@Test
-	void test_unhandle_case_1() {
+	void test_error_3_unhandle_case_1() {
 		_setup();
 		_run_script();
 	}
