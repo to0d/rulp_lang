d5d6966789f4689909c07c34b42d9e3c7307dce3
todd-t4 <lylybj@cn.ibm.com>
2021-12-23 16:58:28 +0800

    up

--- test/beta/rulp/optimize/TestOptimizeSCO.java
+++ test/beta/rulp/optimize/TestOptimizeSCO.java
@@ -7,10 +7,16 @@ import alpha.rulp.utils.RulpTestBase;
 public class TestOptimizeSCO extends RulpTestBase {
 
 	@Test
-	public void test_cc0_1_simple_expr_1() {
+	public void test_cc0_1_num_expr_1() {
 
 		_setup();
 		_run_script();
 	}
 
+	@Test
+	public void test_cc0_1_num_expr_2() {
+
+		_setup();
+		_run_script();
+	}
 }
