fb1f1eb5a5744b64fb6b7c1987293cbb7b9aa1d1
todd <to0d@outlook.com>
2021-12-24 10:52:40 +0800

    up

---
+++ test/beta/rulp/optimize/TestOptimizeCC1.java
@@ -0,0 +1,15 @@
+package beta.rulp.optimize;
+
+import org.junit.Test;
+
+import alpha.rulp.utils.RulpTestBase;
+
+public class TestOptimizeCC1 extends RulpTestBase {
+
+	@Test
+	public void test_cc1_1_stable_fun() {
+
+		_setup();
+		_run_script();
+	}
+}
