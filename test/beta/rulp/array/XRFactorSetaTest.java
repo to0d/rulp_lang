683bd9a20cc8a2521b19903134c9bd80d8c91e12
todd <to0d@outlook.com>
2021-12-26 11:20:08 +0800

    up

--- test/beta/rulp/array/XRFactorSetaTest.java
+++ test/beta/rulp/array/XRFactorSetaTest.java
@@ -7,13 +7,15 @@ import alpha.rulp.utils.RulpTestBase;
 class XRFactorSetaTest extends RulpTestBase {
 
 	@Test
-	public void test_seta_1() {
+	public void test_seta_1_array() {
+
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	public void test_seta_2() {
+	public void test_seta_2_list() {
+
 		_setup();
 		_run_script();
 
