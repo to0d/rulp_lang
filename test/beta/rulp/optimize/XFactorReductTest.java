77c3d53414becb4d3effbdfba60843d15be2b6c2
todd <to0d@outlook.com>
2021-12-26 11:02:34 +0800

    up

--- test/beta/rulp/optimize/XFactorReductTest.java
+++ test/beta/rulp/optimize/XFactorReductTest.java
@@ -4,13 +4,47 @@ import org.junit.jupiter.api.Test;
 
 import alpha.rulp.utils.RulpTestBase;
 
-class XFactorReductTest  extends RulpTestBase {
+class XFactorReductTest extends RulpTestBase {
 
 	@Test
-	public void test_reduct_1_num_expr_1() {
+	public void test_reduct_1_num_1_const() {
 
 		_setup();
 		_run_script();
 	}
 
+	@Test
+	public void test_reduct_2_str_1_const() {
+
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	public void test_reduct_3_logical_1_const() {
+
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	public void test_reduct_3_logical_2_var() {
+
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	public void test_reduct_4_multi_1_unchanged_var() {
+
+		_setup();
+		_run_script();
+	}
+	
+	@Test
+	public void test_reduct_4_multi_2_div_var() {
+
+		_setup();
+		_run_script();
+	}
 }
