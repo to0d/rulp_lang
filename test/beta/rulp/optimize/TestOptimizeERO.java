6cece716aa30cecbd4c6529707cb81e9189f7410
todd-t4 <lylybj@cn.ibm.com>
2021-12-28 23:24:22 +0800

    up

--- test/beta/rulp/optimize/TestOptimizeERO.java
+++ test/beta/rulp/optimize/TestOptimizeERO.java
@@ -4,87 +4,87 @@ import org.junit.Test;
 
 import alpha.rulp.utils.RulpTestBase;
 
-public class TestOptimizeCC0 extends RulpTestBase {
+public class TestOptimizeERO extends RulpTestBase {
 
 	@Test
-	public void test_cc0_1_num_expr_1() {
+	public void test_ero_1_num_expr_1() {
 
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	public void test_cc0_1_num_expr_2() {
+	public void test_ero_1_num_expr_2() {
 
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	public void test_cc0_2_str_expr_1() {
+	public void test_ero_2_str_expr_1() {
 
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	public void test_cc0_3_recursion_fun_1a() {
+	public void test_ero_3_recursion_fun_1a() {
 
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	public void test_cc0_3_recursion_fun_1b() {
+	public void test_ero_3_recursion_fun_1b() {
 
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	public void test_cc0_4_if_1() {
+	public void test_ero_4_if_1() {
 
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	public void test_cc0_5_case_1() {
+	public void test_ero_5_case_1() {
 
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	public void test_cc0_6_do_1() {
+	public void test_ero_6_do_1() {
 
 		_setup();
 		_run_script();
 	}
-	
+
 	@Test
-	public void test_cc0_7_loop_1() {
+	public void test_ero_7_loop_1() {
 
 		_setup();
 		_run_script();
 	}
-	
+
 	@Test
-	public void test_cc0_7_loop_2() {
+	public void test_ero_7_loop_2() {
 
 		_setup();
 		_run_script();
 	}
-	
+
 	@Test
-	public void test_cc0_7_loop_3() {
+	public void test_ero_7_loop_3() {
 
 		_setup();
 		_run_script();
 	}
-	
+
 	@Test
-	public void test_cc0_7_loop_4_infinite_loop_detected() {
+	public void test_ero_7_loop_4_infinite_loop_detected() {
 
 		_setup();
 		_run_script();
