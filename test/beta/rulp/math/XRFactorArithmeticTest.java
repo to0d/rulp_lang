28ec5900e815f81c5ee2b46dc1432da950075849
todd-t4 <lylybj@cn.ibm.com>
2021-12-29 21:35:09 +0800

    up

--- test/beta/rulp/math/XRFactorArithmeticTest.java
+++ test/beta/rulp/math/XRFactorArithmeticTest.java
@@ -11,7 +11,7 @@ class XRFactorArithmeticTest extends RulpTestBase {
 		_setup();
 		_run_script();
 	}
-	
+
 	@Test
 	void test_arithmetic_1_add_2_str() {
 		_setup();
@@ -24,58 +24,75 @@ class XRFactorArithmeticTest extends RulpTestBase {
 		_run_script();
 	}
 
-	
 	@Test
 	void test_arithmetic_2_sub() {
 		_setup();
-		_run_script(); 
+		_run_script();
 	}
 
 	@Test
-	void test_arithmetic_3_by() {
+	void test_arithmetic_3_by_1() {
 		_setup();
-		_run_script();  
+		_run_script();
+	}
+
+	@Test
+	void test_arithmetic_3_by_2_opt_0() {
+		_setup();
+		_run_script();
 	}
 
 	@Test
 	void test_arithmetic_4_div() {
 		_setup();
-		_run_script();   
+		_run_script();
 	}
 
 	@Test
 	void test_arithmetic_5_mod() {
 		_setup();
-		_run_script();  
+		_run_script();
 	}
 
 	@Test
-	void test_arithmetic_6_power() {
+	void test_arithmetic_6_power_1() {
 		_setup();
-		_run_script(); 
+		_run_script();
+	}
+
+	@Test
+	void test_arithmetic_6_power_2_opt_0() {
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	void test_arithmetic_6_power_2_opt_1() {
+		_setup();
+		_run_script();
 	}
 
 	@Test
 	void test_arithmetic_7_and() {
 		_setup();
-		_run_script();  
+		_run_script();
 	}
 
 	@Test
 	void test_arithmetic_8_or() {
 		_setup();
-		_run_script();   
+		_run_script();
 	}
 
 	@Test
 	void test_arithmetic_9_xor() {
 		_setup();
-		_run_script();   
+		_run_script();
 	}
 
 	@Test
 	void test_arithmetic_a_not() {
 		_setup();
-		_run_script(); 
+		_run_script();
 	}
 }
