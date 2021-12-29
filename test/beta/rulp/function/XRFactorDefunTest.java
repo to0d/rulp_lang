35bfc1bcfca8f53a5c2010adf89ef62a2ab6ed40
todd-t4 <lylybj@cn.ibm.com>
2021-12-29 10:12:33 +0800

    up

---
+++ test/beta/rulp/function/XRFactorDefunTest.java
@@ -0,0 +1,147 @@
+package beta.rulp.function;
+
+import static org.junit.Assert.fail;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import alpha.rulp.lang.RException;
+import alpha.rulp.utils.RulpTestBase;
+import alpha.rulp.utils.RulpUtil;
+
+class XRFactorDefunTest extends RulpTestBase {
+
+	@Test
+	void test_fun_0_bad_para_1_undfine_type_1() {
+		_setup();
+		_run_script();
+
+	}
+
+	@Test
+	void test_fun_0_bad_para_1_dup_para_1() {
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	void test_fun_1() {
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	void test_fun_2_ref_1() {
+
+		_setup();
+		_run_script();
+
+	}
+
+	@Test
+	void test_fun_2_ref_2() {
+
+		_setup();
+		_run_script();
+
+	}
+
+	@Test
+	void test_fun_3_overload_arg_number() {
+
+		_setup();
+		_run_script();
+
+	}
+
+	@Test
+	void test_fun_3_overload_arg_type_1() {
+
+		_setup();
+		_run_script();
+
+	}
+
+	@Test
+	void test_fun_3_overload_arg_type_2() {
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	void test_fun_3_overload_cross_frame_1_override() {
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	void test_fun_3_overload_cross_frame_1_override2() {
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	void test_fun_3_overload_cross_frame_2() {
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	void test_fun_3_overload_null_arg() {
+
+		_setup();
+
+		try {
+			RulpUtil.addFactor(_getInterpreter().getMainFrame(), "get-null", (_args, _interpreter, _frame) -> {
+				return null;
+			});
+		} catch (RException | IOException e) {
+			e.printStackTrace();
+			fail(e.toString());
+		}
+
+		_run_script();
+	}
+
+	@Test
+	void test_fun_4_scope_a() {
+
+		_setup();
+		_run_script();
+
+	}
+
+	@Test
+	void test_fun_4_scope_b() {
+
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	void test_fun_4_scope_c() {
+
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	void test_fun_4_scope_d() {
+
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	void test_fun_5_pass_expr() {
+		_setup();
+		_run_script();
+	}
+
+	@Test
+	void test_fun_6_extend_body() {
+		_setup();
+		_run_script();
+	}
+}
