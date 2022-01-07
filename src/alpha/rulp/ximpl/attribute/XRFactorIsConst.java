0182f7302ec7ca733c122b80a86c5222345f1ac6
todd-t4 <lylybj@cn.ibm.com>
2022-01-07 10:37:09 +0800

    up

--- src/alpha/rulp/ximpl/attribute/XRFactorIsConst.java
+++ src/alpha/rulp/ximpl/attribute/XRFactorIsConst.java
@@ -7,7 +7,7 @@
 /* This is free software, and you are welcome to     */
 /* redistribute it under certain conditions.         */
 
-package alpha.rulp.ximpl.factor;
+package alpha.rulp.ximpl.attribute;
 
 import static alpha.rulp.lang.Constant.O_False;
 import static alpha.rulp.lang.Constant.O_True;
@@ -18,6 +18,7 @@ import alpha.rulp.lang.IRObject;
 import alpha.rulp.lang.RException;
 import alpha.rulp.runtime.IRFactor;
 import alpha.rulp.runtime.IRInterpreter;
+import alpha.rulp.ximpl.factor.AbsAtomFactorAdapter;
 
 public class XRFactorIsConst extends AbsAtomFactorAdapter implements IRFactor {
 
