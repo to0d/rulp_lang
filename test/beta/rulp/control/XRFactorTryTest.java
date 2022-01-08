e64e454be3a3fc21bac4152ebdf56ea50b0cc9cc
todd <to0d@outlook.com>
2022-01-08 21:51:56 +0800

    up

--- test/beta/rulp/control/XRFactorTryTest.java
+++ test/beta/rulp/control/XRFactorTryTest.java
@@ -4,23 +4,23 @@ import org.junit.jupiter.api.Test;
 
 import alpha.rulp.utils.RulpTestBase;
 
-class XRFactorErrorTest extends RulpTestBase {
+class XRFactorTryTest extends RulpTestBase {
 
 	@Test
-	void test_error_1_default_handle_case() {
+	void test_try_1_default() {
 
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	void test_error_2_user_define_handle_case() {
+	void test_try_2_user_define() {
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	void test_error_3_unhandle_case_1() {
+	void test_try_3_unhandle() {
 		_setup();
 		_run_script();
 	}
