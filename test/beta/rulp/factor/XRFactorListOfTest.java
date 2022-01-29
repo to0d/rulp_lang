ccff5a040278f97802dbe153251b0b6f8852aa9f
todd <to0d@outlook.com>
2022-01-29 17:26:51 +0800

    up

--- test/beta/rulp/factor/XRFactorListOfTest.java
+++ test/beta/rulp/factor/XRFactorListOfTest.java
@@ -4,31 +4,31 @@ import org.junit.jupiter.api.Test;
 
 import alpha.rulp.utils.RulpTestBase;
 
-public class XRFactorLsTest extends RulpTestBase {
+public class XRFactorListOfTest extends RulpTestBase {
 
 	@Test
-	public void test_ls_1() {
+	public void test_list_of_1() {
 
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	public void test_ls_3_fun() {
+	public void test_list_of_3_fun() {
 
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	public void test_ls_4_env_vars() {
+	public void test_list_of_4_env_vars() {
 
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	public void test_ls_5_usr_vars() {
+	public void test_list_of_5_usr_vars() {
 
 		_setup();
 		_run_script();
