afb503bdf28e0c6a75d6e67afe10761bc71908a5
todd <to0d@outlook.com>
2022-01-29 17:29:05 +0800

    up

--- src/alpha/rulp/ximpl/time/XRFactorDate.java
+++ src/alpha/rulp/ximpl/time/XRFactorDate.java
@@ -9,11 +9,6 @@
 
 package alpha.rulp.ximpl.time;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
 import alpha.rulp.lang.IRFrame;
 import alpha.rulp.lang.IRList;
 import alpha.rulp.lang.IRObject;
@@ -22,15 +17,10 @@ import alpha.rulp.runtime.IRFactor;
 import alpha.rulp.runtime.IRInterpreter;
 import alpha.rulp.utils.RulpFactory;
 import alpha.rulp.utils.RulpUtil;
+import alpha.rulp.utils.XDay;
 import alpha.rulp.ximpl.factor.AbsAtomFactorAdapter;
-import alpha.rulp.ximpl.runtime.XRInterpreter;
 
 public class XRFactorDate extends AbsAtomFactorAdapter implements IRFactor {
-
-	static SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-	static {
-		dateFormat.setLenient(false);
-	}
 
 	public XRFactorDate(String factorName) {
 		super(factorName);
@@ -43,36 +33,14 @@ public class XRFactorDate extends AbsAtomFactorAdapter implements IRFactor {
 			throw new RException("Invalid parameters: " + args);
 		}
 
-		int year = 0;
-		int month = 0;
-		int day = 0;
-
 		if (args.size() == 2) {
 
 			String dayString = RulpUtil.asString(interpreter.compute(frame, args.get(1))).asString();
-
-			try {
-
-				Date date = dateFormat.parse(dayString.trim());
-				return RulpFactory.createString(dateFormat.format(date));
-
-			} catch (ParseException e) {
-				if (XRInterpreter.TRACE) {
-					e.printStackTrace();
-				}
-
-				throw new RException(e.toString());
-			}
+			return RulpFactory.createString(XDay.getDay(dayString).toString());
 
 		} else {
 
-			Calendar cal = Calendar.getInstance();
-			year = cal.get(Calendar.YEAR);
-			month = cal.get(Calendar.MONTH) + 1;
-			day = cal.get(Calendar.DATE);
-
-			return RulpFactory.createString(String.format("%4d-%02d-%02d", year, month, day));
-
+			return RulpFactory.createString(XDay.today().toString());
 		}
 	}
 
