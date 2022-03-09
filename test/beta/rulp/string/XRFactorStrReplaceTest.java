ba98334ed4505559bbddb4caabd4714250af62ce
todd-t4 <lylybj@cn.ibm.com>
2022-03-09 11:31:54 +0800

    up

--- test/beta/rulp/string/XRFactorStrReplaceTest.java
+++ test/beta/rulp/string/XRFactorStrReplaceTest.java
@@ -7,7 +7,7 @@ import alpha.rulp.utils.RulpTestBase;
 class XRFactorStrReplaceTest extends RulpTestBase {
 
 	@Test
-	void test_replace_1() {
+	void test_str_replace_1() {
 		_setup();
 		_run_script();   
 	}
