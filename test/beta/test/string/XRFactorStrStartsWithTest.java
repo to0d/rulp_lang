1fe536447467b6d9049035bea37e43f03b630a21
todd-t4 <lylybj@cn.ibm.com>
2022-04-15 18:01:43 +0800

    up

--- test/beta/test/string/XRFactorStrStartsWithTest.java
+++ test/beta/test/string/XRFactorStrStartsWithTest.java
@@ -7,7 +7,7 @@ import alpha.rulp.utils.RulpTestBase;
 class XRFactorStrStartsWithTest extends RulpTestBase {
 
 	@Test
-	void test_start_with_1() {
+	void test_str_start_with_1() {
 		_setup();
 		_run_script(); 
 	}
