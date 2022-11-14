1acbaaea8cb78674cb0d9adaf228088926c90dea
todd-t4 <lylybj@cn.ibm.com>
2022-11-14 23:21:48 +0800

    up

--- test/beta/test/load/TestLimits.java
+++ test/beta/test/load/TestLimits.java
@@ -7,15 +7,20 @@ import alpha.rulp.utils.RulpTestBase;
 public class TestLimits extends RulpTestBase {
 
 	@Test
-	void test_limit_list_1() {
+	void test_limit_1_list() {
 		_setup();
 		_run_script();
 	}
 
 	@Test
-	void test_limit_int_max_min_1() {
+	void test_limit_2_int_max_min() {
 		_setup();
 		_run_script();
 	}
 
+	@Test
+	void test_limit_3_long_max_min() {
+		_setup();
+		_run_script();
+	}
 }
